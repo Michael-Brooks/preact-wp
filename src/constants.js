2e3f17750d50409fee9941e942ec17faad573a11
Michael Brooks <me@michaelbrooks.co.uk>
2019-08-30 11:17:02 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const protocol = 'https';
-const baseUrl = 'michaelbrooks.co.uk/wp-json/wp/v2/';
+const baseUrl = 'www.codeinwp.com/wp-json/wp/v2/';
 const appName = 'Michael Brooks';
 const homeTitle = `${appName} | Website Developer | Newton Abbot`;
 const blogTitle = `Blog | ${appName}`;
